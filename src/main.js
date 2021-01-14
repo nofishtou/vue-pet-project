08b5ba3a06cbcd940cc5784b96ac5ee4e6702e84
nofishtou <damnedbyhimself@gmail.com>
2021-01-14 07:11:52 +0200

    feat: add validation for login and registration, add firebase

--- src/main.js
+++ src/main.js
@@ -5,15 +5,39 @@ import './registerServiceWorker';
 import router from './router';
 import store from './store';
 import dateFilter from '@/filters/date.filter';
+import messagePlugin from '@/utils/message.plugin';
 import 'materialize-css/dist/js/materialize.min';
+
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 Vue.config.productionTip = false;
 
+Vue.use(messagePlugin);
 Vue.use(Vuelidate);
 Vue.filter('date', dateFilter);
 
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount('#app');
+firebase.initializeApp({
+  apiKey: "AIzaSyD3lKzaJd7eY3b_chvqn4SL4HRMJWmww6c",
+  authDomain: "vue-pet-project-601f9.firebaseapp.com",
+  projectId: "vue-pet-project-601f9",
+  storageBucket: "vue-pet-project-601f9.appspot.com",
+  messagingSenderId: "531863599016",
+  appId: "1:531863599016:web:147ceaeaf5c7203c2c8f9d",
+  measurementId: "G-L7T81CHMSE"
+});
+
+let app;
+
+firebase.auth().onAuthStateChanged(() => {
+  if(!app) {
+    app = new Vue({
+      router,
+      store,
+      render: (h) => h(App),
+    }).$mount('#app');
+  }
+});
+
+
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,16 +3,53 @@
     <div class="card-content">
       <span class="card-title">Домашняя бухгалтерия</span>
       <div class="input-field">
-        <input id="email" type="text" v-model.trim="email" :class="{invalid: ($v.email.$dirty && !$v.email.email) || ($v.email.$dirty && !$v.email.required)}"/>
+        <input
+          id="email"
+          type="text"
+          v-model.trim="email"
+          :class="{
+            invalid:
+              ($v.email.$dirty && !$v.email.email) ||
+              ($v.email.$dirty && !$v.email.required),
+          }"
+        />
         <label for="email">Email</label>
-        <small class="helper-text invalid" v-if="$v.email.$dirty && !$v.email.required">Поле Email не должно быть пустым</small>
-        <small class="helper-text invalid" v-else-if="$v.email.$dirty && !$v.email.email">Введите корректный Email</small>
+        <small
+          class="helper-text invalid"
+          v-if="$v.email.$dirty && !$v.email.required"
+          >Поле Email не должно быть пустым
+        </small>
+        <small
+          class="helper-text invalid"
+          v-else-if="$v.email.$dirty && !$v.email.email"
+          >Введите корректный Email
+        </small>
       </div>
       <div class="input-field">
-        <input id="password" type="password" class="validate" v-model.trim="password" :class="{invalid: ($v.password.$dirty && !$v.password.minLength) || ($v.password.$dirty && !$v.password.required)}"/>
+        <input
+          id="password"
+          type="password"
+          class="validate"
+          v-model.trim="password"
+          :class="{
+            invalid:
+              ($v.password.$dirty && !$v.password.minLength) ||
+              ($v.password.$dirty && !$v.password.required),
+          }"
+        />
         <label for="password">Пароль</label>
-        <small class="helper-text invalid" v-if="$v.password.$dirty && !$v.password.required" >Введите пароль</small>
-        <small class="helper-text invalid" v-else-if="$v.email.$dirty && !$v.password.minLength">Пароль должен быть {{this.$v.password.$params.minLength.min}} символов. Сейчас он {{password.length}}</small>
+        <small
+          class="helper-text invalid"
+          v-if="$v.password.$dirty && !$v.password.required"
+          >Введите пароль
+        </small>
+        <small
+          class="helper-text invalid"
+          v-else-if="$v.email.$dirty && !$v.password.minLength"
+          >Пароль должен быть
+          {{ this.$v.password.$params.minLength.min }} символов. Сейчас он
+          {{ password.length }}
+        </small>
       </div>
     </div>
     <div class="card-action">
@@ -31,38 +68,47 @@
   </form>
 </template>
 <script>
-import { required, minLength, email } from 'vuelidate/lib/validators';
+import { required, minLength, email } from "vuelidate/lib/validators";
+import messages from "@/utils/messages";
 
 export default {
-  name: 'login',
+  name: "login",
   data: () => ({
-    email: '',
-    password: '',
+    email: "",
+    password: "",
   }),
   validations: {
     email: {
-      email, required
+      email,
+      required,
     },
     password: {
-      required, minLength: minLength(6)
-    }
+      required,
+      minLength: minLength(6),
+    },
   },
   methods: {
-    submitHandler () {
-      if(this.$v.$invalid){
+    async submitHandler() {
+      if (this.$v.$invalid) {
         this.$v.$touch();
         return;
       }
 
       const formData = {
         email: this.email,
-        password: this.password
+        password: this.password,
       };
 
-      console.log(formData);
-
-      this.$router.push('/');
-    }
+      try {
+        await this.$store.dispatch('login', formData);
+        this.$router.push("/");
+      } catch (e) {}
+    },
   },
-}
+  mounted() {
+    if(messages[this.$route.query.message]) {
+      this.$message(messages[this.$route.query.message])
+    }
+  }
+};
 </script>
